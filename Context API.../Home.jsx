a1d63cea6a5105ec767d1764d831f6794dac0726
Anmol Dube <114559810+Anmoldube@users.noreply.github.com>
2024-07-04 18:04:19 +0530

    Create Home.jsx

---
+++ Context API.../Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <div className="text-yellow-100 mt-3">I m Home section</div>
+    </div>
+  );
+}
+
+export default Home;
